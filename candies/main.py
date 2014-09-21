e63473e560f2fc73da99686e750f51495877fe65
lambdanaut <personettelabs@gmail.com>
2014-09-21 16:50:31 -0700

    a

---
+++ candies/main.py
@@ -0,0 +1,56 @@
+import sys
+
+def solve(children):
+    import pdb; pdb.set_trace()
+    children_len = len(children)
+    children_candies = [1 for child in children]
+    pointer = 0
+
+    while True:
+        c_ch = children[pointer]
+        c_ca = children_candies[pointer]
+        if pointer == 0: p_ch = None
+        else:            p_ch = children[pointer-1]
+        try:    n_ch = children[pointer+1]
+        except: n_ch = None
+
+        if p_ch:
+            p_ca = children_candies[pointer-1]
+
+            if p_ch > c_ch and p_ca == c_ca:
+                pointer -= 1
+                continue
+            elif p_ch < c_ch and p_ca >= c_ca:
+                children_candies[pointer] = p_ca + 1
+                continue
+
+        if n_ch:
+            n_ca = children_candies[pointer+1]
+            if c_ch > n_ch and c_ca <= n_ca:
+                children_candies[pointer] = n_ca + 1
+        else:
+            return sum(children_candies)
+
+        pointer += 1
+
+
+def main():
+    # Read input from a file if one is provided
+    # Else read from stdin
+    try:
+        filename = sys.argv[1]
+        data = open(filename, 'rb').readlines()
+    except IndexError:
+        data = sys.stdin.readlines()
+
+    # Extract constants from input
+    children = [int(v) for v in data[1:]]
+
+    answer = solve(children)
+
+    if answer:
+        print(answer)
+
+
+if __name__ == '__main__':
+    main()
